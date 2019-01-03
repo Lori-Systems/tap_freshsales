232761972fc8f979992665c212989113b74b691f
Tisham Dhar <whatnickd@gmail.com>
2019-01-03 11:00:28 +0300

    Start test-suite

---
+++ tap_freshsales/tests/test_tap.py
@@ -0,0 +1,25 @@
+import pytest
+
+def test_sync_contacts_by_filter():
+    """
+    Test sync of contacts, inject data via responses
+    """
+    pass
+
+def test_sync_deals_by_filter():
+    """
+    Test sync of deals, inject data via responses
+    """
+    pass
+
+def test_sync_tasks_by_filter():
+    """
+    Test sync of tasks, inject data via responses
+    """
+    pass
+
+def test_sync_accounts_by_filter():
+    """
+    Test sync of accounts, inject data via responses
+    """
+    pass
