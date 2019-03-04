95826afbb132b7da8d36018d2cbbf7866261f13e
Tisham Dhar <whatnickd@gmail.com>
2019-03-04 12:58:28 +0300

    #6 Add failing test

--- tap_freshsales/tests/test_tap.py
+++ tap_freshsales/tests/test_tap.py
@@ -3,35 +3,38 @@ and check output from stdout via singer.io API calls
 """
 
 import pytest
-from tap_freshsales import discover
+from tap_freshsales import discover, sync_contacts_by_filter
+from tap_freshsales import sync_accounts_by_filter, sync_appointments_by_filter
+from tap_freshsales import sync_deals_by_filter, sync_leads_by_filter
+from tap_freshsales import sync_sales_activities, sync_tasks_by_filter
 
 
 def test_sync_contacts_by_filter():
     """
     Test sync of contacts, inject data via responses
     """
-    pass
+    assert sync_contacts_by_filter(None,None)
 
 
 def test_sync_deals_by_filter():
     """
     Test sync of deals, inject data via responses
     """
-    pass
+    assert sync_deals_by_filter(None,None)
 
 
 def test_sync_tasks_by_filter():
     """
     Test sync of tasks, inject data via responses
     """
-    pass
+    assert sync_tasks_by_filter(None,None)
 
 
 def test_sync_accounts_by_filter():
     """
     Test sync of accounts, inject data via responses
     """
-    pass
+    assert sync_accounts_by_filter(None,None)
 
 
 def test_tap_discover():
