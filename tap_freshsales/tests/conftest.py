ba21e53656af705b2e7b4459d6045a0603dab69d
Tisham Dhar <whatnickd@gmail.com>
2019-03-05 16:15:09 +0300

    #6 Add mock data and responses to test fetching filtered endpoints

---
+++ tap_freshsales/tests/conftest.py
@@ -0,0 +1,27 @@
+
+import pytest
+import tap_freshsales
+import responses
+import os
+
+TEST_DOMAIN = 'testdomain'
+TEST_DIR = os.path.dirname(__file__)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def default_session_fixture():
+    """Monkey patch globals in freshsales tap module
+    """
+
+    import datetime
+    import json
+    tap_freshsales.CONFIG = {}
+    tap_freshsales.CONFIG['start_date'] = str(datetime.datetime.now())
+    tap_freshsales.CONFIG['domain'] = TEST_DOMAIN
+    pytest.TEST_DIR = TEST_DIR
+    pytest.TEST_DOMAIN = TEST_DOMAIN
+    # Globally activated responses from sample test data
+
+    
+
+    
--- tap_freshsales/tests/test_tap.py
+++ tap_freshsales/tests/test_tap.py
@@ -2,39 +2,87 @@
 and check output from stdout via singer.io API calls
 """
 
+import json
+import os
+import responses
 import pytest
 from tap_freshsales import discover, sync_contacts_by_filter
-from tap_freshsales import sync_accounts_by_filter, sync_appointments_by_filter
-from tap_freshsales import sync_deals_by_filter, sync_leads_by_filter
-from tap_freshsales import sync_sales_activities, sync_tasks_by_filter
+from tap_freshsales import sync_accounts_by_filter
+from tap_freshsales import sync_deals_by_filter
+from tap_freshsales import sync_tasks_by_filter
+from tap_freshsales import load_schemas, get_start
 
 
+def test_get_start():
+    """Test obtaining of start time from entity state
+    """
+    assert get_start(None)
+
+
+def test_load_schemas():
+    """Test schema loading by the tap
+    """
+    assert load_schemas()
+
+
+@responses.activate
 def test_sync_contacts_by_filter():
     """
     Test sync of contacts, inject data via responses
     """
-    assert sync_contacts_by_filter(None,None)
+    contact_data = json.load(
+        open(os.path.join(pytest.TEST_DIR, 'mock_data/contacts.json')))
+    contact_url = 'https://{}.freshsales.io/api/contacts/view/1?per_page=100&page=1'.format(
+        pytest.TEST_DOMAIN)
+    responses.add(responses.GET, contact_url,
+                  json=contact_data, status=200, content_type='application/json')
+    assert sync_contacts_by_filter('updated_at', {'id': 1}) is None
+    assert len(responses.calls) == 1
 
 
+@responses.activate
 def test_sync_deals_by_filter():
     """
     Test sync of deals, inject data via responses
     """
-    assert sync_deals_by_filter(None,None)
+    deal_data = json.load(
+        open(os.path.join(pytest.TEST_DIR, 'mock_data/deals.json')))
+    deal_url = 'https://{}.freshsales.io/api/deals/view/1?per_page=100&page=1'.format(
+        pytest.TEST_DOMAIN)
+    responses.add(responses.GET, deal_url,
+                  json=deal_data, status=200, content_type='application/json')
+    assert sync_deals_by_filter('updated_at', {'id': 1}) is None
+    assert len(responses.calls) == 1
 
 
+@responses.activate
 def test_sync_tasks_by_filter():
     """
     Test sync of tasks, inject data via responses
     """
-    assert sync_tasks_by_filter(None,None)
+    task_data = json.load(
+        open(os.path.join(pytest.TEST_DIR, 'mock_data/tasks.json')))
+    task_url = 'https://{}.freshsales.io/api/tasks?filter=open&include=owner,users,targetable&per_page=100&page=1'.format(
+        pytest.TEST_DOMAIN)
+    responses.add(responses.GET, task_url,
+                  json=task_data, status=200, content_type='application/json')
+    assert sync_tasks_by_filter('updated_at', 'open') is None
+    assert len(responses.calls) == 1
 
 
+@responses.activate
 def test_sync_accounts_by_filter():
     """
     Test sync of accounts, inject data via responses
     """
-    assert sync_accounts_by_filter(None,None)
+    sales_account_data = json.load(
+        open(os.path.join(pytest.TEST_DIR, 'mock_data/sales_accounts.json')))
+    sales_account_url = 'https://{}.freshsales.io/api/sales_accounts/view/1?per_page=100&page=1'.format(
+        pytest.TEST_DOMAIN)
+    responses.add(responses.GET, sales_account_url,
+                  json=sales_account_data, status=200, content_type='application/json')
+    assert sync_accounts_by_filter('updated_at', {'id': 1}) is None
+    assert len(responses.calls) == 1
 
 
 def test_tap_discover():
