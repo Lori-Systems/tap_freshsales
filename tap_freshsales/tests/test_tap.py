db37d06ada511b8677cf767bb1d514fff29704de
kagenyi2@gmail.com <kagenyi2@gmail.com>
2019-07-21 15:35:58 +0300

    adding include=owner

--- tap_freshsales/tests/test_tap.py
+++ tap_freshsales/tests/test_tap.py
@@ -6,9 +6,9 @@ import json
 import os
 import responses
 import pytest
-from tap_freshsales import discover, sync_contacts_by_filter
-from tap_freshsales import sync_accounts_by_filter
-from tap_freshsales import sync_deals_by_filter
+from tap_freshsales import discover, sync_contacts_by_filter, sync_contacts_owner
+from tap_freshsales import sync_accounts_by_filter, sync_accounts_owner
+from tap_freshsales import sync_deals_by_filter, sync_deals_owner, sync_leads_owner
 from tap_freshsales import sync_tasks_by_filter
 from tap_freshsales import load_schemas, get_start
 
@@ -41,6 +41,21 @@ def test_sync_contacts_by_filter():
 
 
 @responses.activate
+def test_sync_contacts_owner():
+    """
+    Test sync of contacts owner, inject data via responses
+    """
+    contact_data = json.load(
+        open(os.path.join(pytest.TEST_DIR, 'mock_data/contacts_owner.json')))
+    contact_url = 'https://{}.freshsales.io/api/contacts/1?include=owner&per_page=100&page=1'.format(
+        pytest.TEST_DOMAIN)
+    responses.add(responses.GET, contact_url,
+                  json=contact_data, status=200, content_type='application/json')
+    assert sync_accounts_owner('is_active',{'id': 1}) is None
+    assert len(responses.calls) == 1
+
+
+@responses.activate
 def test_sync_deals_by_filter():
     """
     Test sync of deals, inject data via responses
@@ -54,6 +69,19 @@ def test_sync_deals_by_filter():
     assert sync_deals_by_filter('updated_at', {'id': 1}) is None
     assert len(responses.calls) == 1
 
+@responses.activate
+def test_sync_deals_owner():
+    """
+    Test sync of leads owner, inject data via responses
+    """
+    deal_data = json.load(
+        open(os.path.join(pytest.TEST_DIR, 'mock_data/deal_owner.json')))
+    deal_url = 'https://{}.freshsales.io/api/deals/view/1?include=owner&per_page=100&page=1'.format(
+        pytest.TEST_DOMAIN)
+    responses.add(responses.GET, deal_url,
+                  json=deal_data, status=200, content_type='application/json')
+    assert sync_deals_owner('is_active',{'id': 1}) is None
+    assert len(responses.calls) == 1
 
 @responses.activate
 def test_sync_tasks_by_filter():
@@ -69,6 +97,20 @@ def test_sync_tasks_by_filter():
     assert sync_tasks_by_filter('updated_at', 'open') is None
     assert len(responses.calls) == 1
 
+@responses.activate
+def test_sync_leads_owner():
+    """
+    Test sync of leads owner, inject data via responses
+    """
+    lead_data = json.load(
+        open(os.path.join(pytest.TEST_DIR, 'mock_data/leads.json')))
+    lead_url = 'https://{}.freshsales.io/api/leads/1?include=owner&per_page=100&page=1'.format(
+        pytest.TEST_DOMAIN)
+    responses.add(responses.GET, lead_url,
+                  json=lead_data, status=200, content_type='application/json')
+    assert sync_leads_owner('is_active',{'id': 1}) is None
+    assert len(responses.calls) == 1
+
 
 @responses.activate
 def test_sync_accounts_by_filter():
@@ -82,6 +124,21 @@ def test_sync_accounts_by_filter():
     responses.add(responses.GET, sales_account_url,
                   json=sales_account_data, status=200, content_type='application/json')
     assert sync_accounts_by_filter('updated_at', {'id': 1}) is None
+    assert len(responses.calls) == 1
+
+
+@responses.activate
+def test_sync_accounts_owner():
+    """
+    Test sync of accounts owner, inject data via responses
+    """
+    sales_account_data = json.load(
+        open(os.path.join(pytest.TEST_DIR, 'mock_data/sales_accounts_owner.json')))
+    sales_account_url = 'https://{}.freshsales.io/api/sales_accounts/view/1?include=owner&per_page=100&page=1'.format(
+        pytest.TEST_DOMAIN)
+    responses.add(responses.GET, sales_account_url,
+                  json=sales_account_data, status=200, content_type='application/json')
+    assert sync_accounts_owner('is_active',{'id': 1}) is None
     assert len(responses.calls) == 1
 
 
